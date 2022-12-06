6eecbaa3ebc8081fc79904d4dd531c9d56aaad50
Gabriel Aaron <gaaron2@illinois.edu>
2022-12-06 05:28:44 -0500

    Add 2022 rust day00 as template

---
+++ 2022/rust/day00/src/lib.rs
@@ -0,0 +1,28 @@
+// Reference: https://github.com/ChristopherBiscardi/advent-of-code/tree/main/2022/rust/day-01
+
+pub fn process_part1(input: &str) -> String {
+    "".to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn test_part1() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, "");
+    }
+}
